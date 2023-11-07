db10ba8450f4e36b92338e9343eaa35dd6d053d2
Codeyg12 <codey.gallup@gmail.com>
2023-11-06 18:02:02 -0700

    initalized repo

---
+++ main.js
@@ -0,0 +1,196 @@
+// 1. Find the Smallest and Biggest Numbers
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// Examples
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+// minMax([2334454, 5]) ➞ [5, 2334454]
+// minMax([1]) ➞ [1, 1]
+
+function minMax(arr) {
+    let min = arr[0]
+    let max = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+          }
+          if (arr[i] > max) {
+            max = arr[i];
+          }
+    }
+    return [min, max]
+}
+
+// 2. Add up the Numbers from a Single Number
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+// Examples
+// addUp(4) ➞ 10
+// addUp(13) ➞ 91
+// addUp(600) ➞ 180300
+
+function addUp(num) {
+    let total = 0
+    for (let i = 1; i < num + 1; i++) {
+        total += i
+    }
+    return total
+}
+
+// 3. Left Shift by Powers of Two
+// The left shift operation is similar to multiplication by powers of two.
+
+// Sample calculation using the left shift operator (<<):
+
+// 10 << 3 = 10 * 2^3 = 10 * 8 = 80
+// -32 << 2 = -32 * 2^2 = -32 * 4 = -128
+// 5 << 2 = 5 * 2^2 = 5 * 4 = 20
+// Write a function that mimics (without the use of <<) the left shift operator and returns the result from the two given integers.
+
+// Examples
+// shiftToLeft(5, 2) ➞ 20
+// shiftToLeft(10, 3) ➞ 80
+// shiftToLeft(-32, 2) ➞ -128
+// shiftToLeft(-6, 5) ➞ -192
+// shiftToLeft(12, 4) ➞ 192
+// shiftToLeft(46, 6) ➞ 2944
+
+function shiftToLeft(a, b) {
+    let two = 2
+    let total = 0
+    for (let i = b; i > 1; i--) {
+        two *= 2
+    }
+    total = a * two
+    return total
+}
+
+// 4. RegEx Exercise 1: Find the Time
+// The time has a format: hours:minutes. Both hours and minutes have two digits, like 09:00.
+
+// Make a regexp to find time in the string: Breakfast at 09:00 in the room 123:456. In this task there's no need to check time correctness yet, so 25:99 can also be a valid result. The regexp should not match 123:456.
+
+'\b\d{2}:\d{2}\b'
+
+// 5. Largest Swap
+// Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+
+// To illustrate:
+
+// largestSwap(27) ➞ false
+
+// largestSwap(43) ➞ true
+// If 27 is our input, we should return false because swapping the digits gives us 72, and 72 > 27. On the other hand, swapping 43 gives us 34, and 43 > 34.
+
+// Examples
+// largestSwap(14) ➞ false
+// largestSwap(53) ➞ true
+// largestSwap(99) ➞ true
+
+function largestSwap(num) {
+    let sorted = num.toString().split('')
+    return (sorted[0] >= sorted[1])
+}
+
+// 6. How Much is True?
+// Create a function which returns the number of "true" values there are in an array.
+
+// Examples
+// countTrue([true, false, false, true, false]) ➞ 2
+// countTrue([false, false, false, false]) ➞ 0
+// countTrue([]) ➞ 0
+
+function countTrue(arr) {
+    if (arr.length === 0) return 0
+    let total = 0
+    for (let word of arr) {
+        if (word === true) total++
+    }
+    return total
+}
+
+// 7. A Redundant Function
+// Write a function "redundant" that takes in a string "str" and returns a function that returns "str".
+
+// Examples
+// const f1 = redundant("apple")
+// f1() ➞ "apple"
+
+// const f2 = redundant("pear")
+// f2() ➞ "pear"
+
+// const f3 = redundant("")
+// f3() ➞ ""
+
+const redundant = (str) => {
+    return function() {
+        return str
+    }
+}
+
+// 8. Find Number of Digits in Number
+// Create a function that will return an integer number corresponding to the amount of digits in the given integer num.
+
+// Examples
+// num_of_digits(1000) ➞ 4
+// num_of_digits(12) ➞ 2
+// num_of_digits(1305981031) ➞ 10
+// num_of_digits(0) ➞ 1
+
+function num_of_digits(num) {
+    let total = num.toString().split('').length
+    return total
+}
+
+// 9. Array of Multiples
+// Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+
+// Examples
+// arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+// arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+// arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+
+function arrayOfMultiples(a, b) {
+    let arr = []
+    for (let i = 1; i < b+1; i++) {
+        let newNum = i * a
+        arr.push(newNum)
+    }
+    return arr
+}
+
+// 10. Concatenate Variable Number of Input Arrays
+// Create a function that concatenates n input arrays, where n is variable.
+
+// Examples
+// concat([1, 2, 3], [4, 5], [6, 7]) ➞ [1, 2, 3, 4, 5, 6, 7]
+// concat([1], [2], [3], [4], [5], [6], [7]) ➞ [1, 2, 3, 4, 5, 6, 7]
+// concat([1, 2], [3, 4]) ➞ [1, 2, 3, 4]
+// concat([4, 4, 4, 4, 4]) ➞ [4, 4, 4, 4, 4]
+
+const concat = (...n) => {
+    let arr = []
+    for (let i of n) {
+        arr.push(i)
+    }
+    return arr.flat()
+}
+
+// 11. Sort the Unsortable
+// In this challenge you will be given an array similar to the following:
+
+// [[3], 4, [2], [5], 1, 6]
+// In words, elements of the array are either an integer or an array containing a single integer. We humans can clearly see that this array can reasonably be sorted according to "the content of the elements" as:
+
+// [1, [2], [3], 4, [5], 6]
+// Create a function that, given an array similar to the above, sorts the array according to the "content of the elements".
+
+// Examples
+// sortIt([4, 1, 3]) ➞ [1, 3, 4]
+// sortIt([[4], [1], [3]]) ➞ [[1], [3], [4]]
+// sortIt([4, [1], 3]) ➞ [[1], 3, 4]
+// sortIt([[4], 1, [3]]) ➞ [1, [3], [4]]
+// sortIt([[3], 4, [2], [5], 1, 6]) ➞ [1, [2], [3], 4, [5], 6]
+
+const sortIt = (arr) => {
+    return arr.sort()
+}
