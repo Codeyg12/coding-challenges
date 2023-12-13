35ef64b9a6fa5ca953c7a4853eb10f4b5186bfe6
Codeyg12 <codey.gallup@gmail.com>
2023-12-12 19:54:17 -0700

    testing

--- Node/challenge2/index.js
+++ Node/challenge2/index.js
@@ -1,13 +1,13 @@
 // Challenge 2: Promisify a Callback Function
 // Take a common callback-based function, such as fs.readFile, and promisify it. Create a function that returns a Promise-based version of the callback function.
 const fs = require('fs')
-const { promsifiy } = require('util')
+const { promisify } = require('util')
 
 const inputFile = 'text.txt'
 const outputFile = 'output.txt'
 
-const readFileAsync = promsifiy(fs.readFile)
-const writeFileAsync = promsifiy(fs.writeFile)
+const readFileAsync = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
 
 const promiseFile = async () => {
     try {
