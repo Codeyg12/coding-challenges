61c51dd49b63793b742aaa87aefa2db8450c7c09
Codeyg12 <codey.gallup@gmail.com>
2023-12-18 20:29:40 -0700

    starting new apis to be used

--- Node/challenge3/index.js
+++ Node/challenge3/index.js
@@ -1,2 +1,21 @@
 // Challenge 3: Fetch and Process Data (Promises)
 // Using the Node.js http or axios library, fetch data from an external API (e.g., JSONPlaceholder or any public API). Once the data is retrieved, use Promises to process and display specific information from the response.
+const axios = require("axios");
+const fetchURL = "https://jsonplaceholder.typicode.com/todos/1";
+
+const fetchAndProcess = () => {
+  axios
+    .get(fetchURL)
+    .then((res) => {
+      const data = res.data;
+      const { userId, id, title } = data;
+      console.log(
+        `To Do item #${id} created by user #${userId} is called '${title}'`
+      );
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+};
+
+fetchAndProcess();
--- Node/challenge4/index.js
+++ Node/challenge4/index.js
@@ -1,2 +1,12 @@
 // Challenge 4: Async/Await and API Requests
 // Create an application that uses async/await to make multiple API requests in parallel and aggregate the results. For example, fetch data from two different endpoints and combine the data into a single response.
+let rColor = Math.floor(Math.random() * 255)
+let gColor = Math.floor(Math.random() * 255)
+let bColor = Math.floor(Math.random() * 255)
+let randomUser = Math.floor(Math.random() * 10)
+const fetchURL = `https://jsonplaceholder.typicode.com/users/${randomUser}`;
+const colorURL = `https://www.thecolorapi.com/id?rgb=rgb(${rColor},${gColor},${bColor})`
+
+fetch(colorURL)
+.then(res => res.json())
+.then(h => console.log(h))
