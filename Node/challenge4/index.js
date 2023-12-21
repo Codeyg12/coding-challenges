27384cf4aec2e5d3e67f35420615101bfef5adc8
Codeyg12 <codey.gallup@gmail.com>
2023-12-20 18:30:16 -0700

    challenge4 finished

--- Node/challenge4/index.js
+++ Node/challenge4/index.js
@@ -3,10 +3,22 @@
 let rColor = Math.floor(Math.random() * 255)
 let gColor = Math.floor(Math.random() * 255)
 let bColor = Math.floor(Math.random() * 255)
-let randomUser = Math.floor(Math.random() * 10)
+let randomUser = Math.ceil(Math.random() * 10)
 const fetchURL = `https://jsonplaceholder.typicode.com/users/${randomUser}`;
 const colorURL = `https://www.thecolorapi.com/id?rgb=rgb(${rColor},${gColor},${bColor})`
 
-fetch(colorURL)
-.then(res => res.json())
-.then(h => console.log(h))
+const multiEndpoints = async () => {
+    try {
+        const firstData = await fetch(fetchURL)
+    const user = await firstData.json()
+    const { name, address: {city}, company: {name: companyName} } = user
+    const secondData = await fetch(colorURL)
+    const color = await secondData.json()
+    const { name: {value: colorName }} = color
+    console.log(`Hello, my name is ${name} and I work at ${companyName} located in ${city}. My favorite color is ${colorName}.`);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+multiEndpoints()
