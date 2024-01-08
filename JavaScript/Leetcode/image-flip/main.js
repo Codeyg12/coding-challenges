dd8bd24b93f53b1ead0d4d2e947df324ed16ff7e
Codeyg12 <codey.gallup@gmail.com>
2024-01-08 14:36:51 -0700

    image flip done

--- JavaScript/Leetcode/image-flip/main.js
+++ JavaScript/Leetcode/image-flip/main.js
@@ -1,5 +1,17 @@
 const flipAndInvertImage = (image) => {
-    console.log(image);
+    console.log('?',image);
+    for (const arr of image) {
+        arr.reverse()
+        for (let num in arr) {
+            if (arr[num] == 0) {
+                arr[num] = 1
+            } else {
+                arr[num] = 0
+            }
+        }
+    }
+    console.log('Answer',image);
+    return image
 }
 
 flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])
