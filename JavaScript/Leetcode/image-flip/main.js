013e529d7b5ad2ddbe0d3b75c51bd20709aee6cf
Codeyg12 <codey.gallup@gmail.com>
2024-01-08 14:20:09 -0700

    floodfill finish

--- JavaScript/Leetcode/image-flip/main.js
+++ JavaScript/Leetcode/image-flip/main.js
@@ -1,3 +1,10 @@
 const flipAndInvertImage = (image) => {
-    
+    console.log(image);
 }
+
+flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])
+// Output: [[1,0,0],[0,1,0],[1,1,1]]
+
+
+flipAndInvertImage([[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]])
+// Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]  
