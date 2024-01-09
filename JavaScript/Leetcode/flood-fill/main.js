8708cf56d3c8e19230598478ae568b876d578389
Codeyg12 <codey.gallup@gmail.com>
2024-01-08 17:33:33 -0700

    formatting

--- JavaScript/Leetcode/flood-fill/main.js
+++ JavaScript/Leetcode/flood-fill/main.js
@@ -1,29 +1,45 @@
 const floodFill = (image, sr, sc, color) => {
-    console.log('start', image);
-    if (image[sr][sc] === color) return image
-    let startingRow = image[sr]
-    let oldColor = startingRow[sc]
-   
-    navigateGrid(image, sr, sc, oldColor, color)
-    console.log('finish',image);
-}
+  console.log("start", image);
+  if (image[sr][sc] === color) return image;
+  let startingRow = image[sr];
+  let oldColor = startingRow[sc];
+
+  navigateGrid(image, sr, sc, oldColor, color);
+  console.log("finish", image);
+};
 
 const navigateGrid = (image, row, col, oldColor, newColor) => {
-    if (row < 0 || row >= image.length || col < 0 || col >= image[0].length || image[row][col] !== oldColor) return
-    
-    image[row][col] = newColor
+  if (
+    row < 0 ||
+    row >= image.length ||
+    col < 0 ||
+    col >= image[0].length ||
+    image[row][col] !== oldColor
+  )
+    return;
 
-    navigateGrid(image, row - 1, col, oldColor, newColor) 
-    navigateGrid(image, row + 1, col, oldColor, newColor) 
-    navigateGrid(image, row, col - 1, oldColor, newColor) 
-    navigateGrid(image, row, col + 1, oldColor, newColor) 
-}
+  image[row][col] = newColor;
+
+  navigateGrid(image, row - 1, col, oldColor, newColor);
+  navigateGrid(image, row + 1, col, oldColor, newColor);
+  navigateGrid(image, row, col - 1, oldColor, newColor);
+  navigateGrid(image, row, col + 1, oldColor, newColor);
+};
 
 // image will be the array of arays
 // sr will be the index of the array of arrays
 // sc is the index of the single array
 // color is what everything changes too
 
-floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2)
+floodFill(
+  [
+    [1, 1, 1],
+    [1, 1, 0],
+    [1, 0, 1],
+  ],
+  1,
+  1,
+  2
+);
 
 // Output: [[2,2,2],[2,2,0],[2,0,1]]
--- JavaScript/Leetcode/image-flip/main.js
+++ JavaScript/Leetcode/image-flip/main.js
@@ -1,17 +1,25 @@
 const flipAndInvertImage = (image) => {
-    for (const arr of image) {
-        arr.reverse()
-        for (let i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] === 0 ? 1 : 0
-        }
+  for (const arr of image) {
+    arr.reverse();
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i] === 0 ? 1 : 0;
     }
-    console.log(image);
-    return image
-}
+  }
+  console.log(image);
+  return image;
+};
 
-flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])
+flipAndInvertImage([
+  [1, 1, 0],
+  [1, 0, 1],
+  [0, 0, 0],
+]);
 // Output: [[1,0,0],[0,1,0],[1,1,1]]
 
-
-flipAndInvertImage([[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]])
-// Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]  
+flipAndInvertImage([
+  [1, 1, 0, 0],
+  [1, 0, 0, 1],
+  [0, 1, 1, 1],
+  [1, 0, 1, 0],
+]);
+// Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
